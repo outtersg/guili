dc36350557078e917a4c515265b16c5541554129
Guillaume Outters <guillaume-installs@outters.eu>
2011-06-13 20:17:34 +0200

    lftp.sh: sutiliser
    = sutiliser pour portabilité.
    
    darcs-hash:89702e9b23e6aa05790fa6e248780938ece17349

--- lftp.sh
+++ lftp.sh
@@ -6,5 +6,5 @@ cd /tmp
 tar xjf `obtenir ftp://ftp.cs.tu-berlin.de/pub/net/ftp/lftp/lftp-3.0.6.tar.bz2`
 cd lftp-3.0.6
 ./configure --prefix=/usr/local/lftp-3.0.6
-make && sudo make install && sudo utiliser lftp-3.0.6
+make && sudo make install && sutiliser lftp-3.0.6
 
