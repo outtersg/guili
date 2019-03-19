fd4f8d87c071c88ca3bfd5621636692ec09b9c98
Guillaume Outters <guillaume-installs@outters.eu>
2019-03-19 22:44:06 +0100

    util.python: installation d'extensions Python
    + Utilitaires pour aider les extensions Python à s'installer.
    # Tant qu'à faire, on essaie de les centraliser dans le même dossier d'$INSTALLS (pour qu'on n'ait que ce dossier à ajouter au $PYTHONPATH, et non pas celui de chaque extension), sans que ce soit le dossier d'install de Python lui-même; d'une pour ne pas le pourrir, de deux pour que si on fait une montée en version mineure de Python les extensions préalablement installées soient encore là.
    
    darcs-hash:ce137314a03fb2b28d22476554797411bbd227d9

---
+++ util.python.sh
@@ -0,0 +1,69 @@
+# Copyright (c) 2018-2019 Guillaume Outters
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+pyp()
+{
+	local pvc="`python --version 2>&1 | sed -e 's/[^0-9]*\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/'`" # Python version courte
+	echo "lib/python$pvc/site-packages"
+}
+
+# Renvoie un PYTHONPATH incluant les lib/pythonxx/site-packages de $dest, $INSTALLS/$PYEXT,  et $destpython.
+pypadest()
+{
+	local pyp="`pyp`"
+	echo "$dest/$pyp:$INSTALLS/$PYEXT/$pyp:$destpython/$pyp"
+}
+
+# Prépare un dossier à recevoir une installation d'une extension Python.
+pyextinit()
+{
+	local dest="$dest"
+	[ -z "$1" ] || dest="$1"
+	mkdir -p "$dest/`pyp`"
+	export PYTHONPATH="`pypadest`"
+	guili_localiser="$guili_localiser finaliserInstallPyExt"
+}
+
+sudokupy()
+{
+	sudoku LD_LIBRARY_PATH="$LD_LIBRARY_PATH" PYTHONPATH="`pypadest`" SSL_CERT_FILE="$SSL_CERT_FILE" "$@"
+}
+
+pylanceur()
+{
+	local lance="$1"
+	local chaineAbs="`egrep 'absolutiseScripts\(|delie\(' < "$install_moi"`"
+	cat <<TERMINE
+#!/bin/sh
+$chaineAbs
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+export PYTHONPATH="`pypadest`"
+export SSL_CERT_FILE="$SSL_CERT_FILE"
+"\$SCRIPTS/$lance" "\$@"
+TERMINE
+}
+
+finaliserInstallPyExt()
+{
+	local pyext="$INSTALLS/$PYEXT"
+	( cd "$dest" && tar cf - . ) | ( sudoku mkdir -p "$pyext" && cd "$pyext" && sudoku tar xf - )
+}
+
+PYEXT="pyext"
