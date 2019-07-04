72e06c69c4f62dc24fcb88d49387e4b2e27e8e13
Guillaume Outters <guillaume-installs@outters.eu>
2019-07-04 07:41:33 +0200

    BOGUE: util.guili: virerPrerequis: ne savait pas virer les logiciel+option
    # Le + était passé tel quel à sed, qui le prenait mal.
    
    darcs-hash:db3bf780ea7c08ebf063a64f44c5630b09c58a8b

--- util.guili.sh
+++ util.guili.sh
@@ -84,7 +84,7 @@ decoupePrerequis()
 # Il est suggéré de fonctionner de cette façon (déclarer le maximum de prérequis, et trouilloter ensuite si telle ou telle condition n'est finalement pas remplie sur la plate-forme, ou si le logiciel n'est pas demandé par une option explicite), car ainsi pour chaque version du logiciel (fonctions v()) on peut définir dans quelle version le prérequis devra être inclus au cas où il reste en lice.
 virerPrerequis()
 {
-	local aVirer="`echo "$*" | tr ' ' '|'`"
+	local aVirer="`echo "$*" | sed -e 's/[+]/\\\\+/g' -e 's/ /|/g'`"
 	prerequis="`echo " $prerequis " | sed -E -e 's/ /  /g' -e "s# ($aVirer)([ <=>0-9.]+)* # #g"`"
 }
 
