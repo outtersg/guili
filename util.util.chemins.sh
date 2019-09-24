caa6aa71e6d75fa3dfbb66ad9bc8b31cea0a4ecb
Guillaume Outters <guillaume-installs@outters.eu>
2019-09-25 00:45:19 +0200

    util.util.chemins: ln_sr
    + Sorte de ln -s, mais capable de retrouver le chemin relatif pour atteindre la cible depuis le lien.
    
    darcs-hash:d4f5260c6772ff165c403c5714d190be3b9a7bee

--- util.util.chemins.sh
+++ util.util.chemins.sh
@@ -17,3 +17,93 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+# Crée un lien symbolique relatif vers un autre chemin.
+# Utilisation: ln_sr [-e <var>] [-a] <original> <lien>
+#   -e <var>
+#     Externalise. Le ln -s final n'est pas tiré, à la place le résultat est mis dans la variable $<var>.
+#   -a
+#     Absolu. On ne tente pas de remonter le chemin <lien> pour attendre <original> par lien relatif.
+#   <original>
+#     Chemin sur lequel pointer.
+#   <lien>
+#     Chemin qui pointera.
+ln_sr()
+{
+	local var= abs=
+	while [ $# -gt 0 ]
+	do
+		case "$1" in
+			-e) var="$2" ; shift ;;
+			-a) abs=oui ;;
+			*) break ;;
+		esac
+		shift
+	done
+	
+	local o="$1" l="$2" dl
+	case "$o" in
+		[^/]*) o="$PWD/$o" ;;
+	esac
+	case "$l" in
+		[^/]*) o="$PWD/$l" ;;
+	esac
+	if [ -d "$l" ]
+	then
+		dl="$l"
+		l="$l/`basename "$o"`"
+	else
+		dl="`dirname "$l"`"
+	fi
+	if [ -z "$abs" ]
+	then
+		IFS=/
+		_ln_sr_relativiser o "$dl" $o
+		unset IFS
+	fi
+	
+	if [ -n "$var" ]
+	then
+		if [ "x$var" = x- ]
+		then
+			echo "$o"
+		else
+			eval $var="\"\$o\""
+		fi
+	else
+		ln -s "$o" "$l"
+	fi
+}
+
+_ln_sr_relativiser()
+{
+	local var="$1" ; shift
+	local accu=
+	local l="$1" ; shift
+	local bl
+	local diff=
+	# Supprimons la partie commune.
+	for bl in $l
+	do
+		case "$bl" in
+			""|.) true ;;
+			..) echo "# ln_sr: je ne sais pas gérer le .. dans le chemin." >&2 ; return 1 ;;
+			*)
+				while [ -z "$diff" ]
+				do
+					case "$1" in
+						""|.) shift ;;
+						..) echo "# ln_sr: je ne sais pas gérer le .. dans le chemin." >&2 ; return 1 ;;
+						"$bl") shift ; break ;;
+						*) diff=1 ;;
+					esac
+				done
+				if [ -n "$diff" ]
+				then
+					accu="$accu../"
+				fi
+				;;
+		esac
+	done
+	eval $var="\"\$accu\$*\""
+}
