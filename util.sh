bec5d6f91d6f0d7f412f434211dfd85dbc83fd0b
Guillaume Outters <guillaume-installs@outters.eu>
2019-10-25 23:05:04 +0200

    util.versions
    - Les fonctions de travail sur les versions sont sorties d'util.sh; mais ne sont pas mises dans util.util, qui devient un peu fourre-tout.
    
    darcs-hash:3cf5e99279f52e1197e37fe0d0db31d2bc013cd3

--- util.sh
+++ util.sh
@@ -280,6 +280,7 @@ chemins_init=cheminsGuili
 . "$SCRIPTS/util.args.sh"
 . "$SCRIPTS/util.guili.sed.sh" # Va nous servir pour beaucoup de choses.
 . "$SCRIPTS/util.util.sh"
+. "$SCRIPTS/util.versions.sh"
 
 # Remplacements de commandes (pour la phase d'amorçage).
 
@@ -722,93 +723,6 @@ v()
 	v="`echo "$1" | sed -e 's/@.*//'`"
 	testerVersion "$v" $argVersion && version="$v" && versionComplete="$1"
 	testerVersion "$v" ppe $argVersion
-}
-
-# Teste si la version mentionnée en premier paramètre rentre (ou est plus petite ou égale, si le second paramètre vaut 'ppe') dans l'intervalle défini par la suite des arguments (ex.: testerVersion 2.3.1 >= 2.3 < 2.4 renverra vrai).
-testerVersion()
-{
-	[ "x$1" = x ] && return 0 # Sans mention de version, on dit que tout passe.
-	versionTestee="$1"
-	shift
-	plusPetitOuEgal=false
-	[ "x$1" = xppe ] && plusPetitOuEgal=true && shift
-	while [ $# -gt 0 ]
-	do
-		case "$1" in
-			">=")
-				$plusPetitOuEgal || pge "$versionTestee" "$2" || return 1 # Si on teste un PPE, le >= n'est pas filtrant (la clause PPE est prioritaire).
-				shift
-				;;
-			"<")
-				pg "$2" "$versionTestee" || return 1
-				shift
-				;;
-			*) # Numéro de version précis.
-				if $plusPetitOuEgal
-				then
-					pge "$1" "$versionTestee" || return 1
-				else
-					[ "$versionTestee" = "$1" ] || return 1
-				fi
-				;;
-		esac
-		shift
-	done
-	true
-}
-
-pge() { pg -e "$1" "$2" ; }
-
-triversions()
-{
-	# De deux logiciels en même version, on prend le chemin le plus long: c'est celui qui embarque le plus de modules optionnels.
-	awk '
-		BEGIN {
-			# Certaines versions d awk veulent que ls soit initialisée en array avant de pouvoir être length()ée.
-			nls = 0;
-			nvs = 0;
-			ntailles = 0;
-		}
-		{
-			ls[++nls] = $0;
-			v = $0;
-			sub(/^([^0-9][^-]*-)+/, "", v);
-			vs[++nvs] = v;
-			ndecoupe = split(v, decoupe, ".");
-			for(i = 0; ++i <= ndecoupe;)
-			{
-				if(i > ntailles)
-				{
-					++ntailles;
-					tailles[i] = 0;
-				}
-				if(length(decoupe[i]) > tailles[i])
-					tailles[i] = length(decoupe[i]);
-			}
-		}
-		END {
-			for(nl = 0; ++nl <= nvs;)
-			{
-				c = "";
-				v = vs[nl];
-				ndecoupe = split(v, decoupe, ".");
-				for(nv = 0; ++nv <= ntailles;)
-					c = c sprintf("%0"tailles[nv]"d", nv > ndecoupe ? 0 : decoupe[nv]);
-				print c" "sprintf("%04d", length(ls[nl]))" "ls[nl]
-			}
-		}
-	' | sort | cut -d ' ' -f 3-
-}
-
-filtrerVersions()
-{
-	sed -e '/^.*-\([0-9.]*\)$/!d' -e 's##\1 &#' | while read v chemin
-	do
-		if testerVersion "$v" $@
-		then
-			echo "$chemin"
-		fi
-	done
 }
 
 ### Fonctions utilitaires dans le cadre des modifs. ###
