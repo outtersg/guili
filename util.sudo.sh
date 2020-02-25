4d2bb4660f0b3e69ead38af6d0ccd4b17a369aef
Guillaume Outters <guillaume-installs@outters.eu>
2020-02-25 21:22:38 +0100

    util.sudo: droits d'auteur
    = 2010-2011,2016-2019 pour accueillir les fonctions sudo développées ces années-là mais dans util.sh.
    
    darcs-hash:0256a5bb9ae464ff105d6bf912a7a25bd6d0107b

--- util.sudo.sh
+++ util.sudo.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2016,2018-2019 Guillaume Outters
+# Copyright (c) 2010-2011,2016-2019 Guillaume Outters
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
