4f8c84c950e4e99700baf653208c1affd50bea9d
Guillaume Outters <guillaume-installs@outters.eu>
2022-02-05 14:35:51 +0100

    util.compilo: langcxx()
    = Remplace les cxx1x(), dans le but d'uniformiser.
    
    darcs-hash:64da1f4693f17621096c042ffceeecc85865844a

--- util.compilo.sh
+++ util.compilo.sh
@@ -367,6 +367,15 @@ langc()
 meilleurCompilo() { langc ; }
 meilleurCompiloInstalle() { meilleurCompilo "$@" ; }
 
+langcxx()
+{
+	# À FAIRE: pour le moment on s'ajoute encore à COMPILO_AJOUTS (au lieu d'un COMPILO_cxx_AJOUTS).
+	case "$1" in
+		""|11|14|17) cxx$1 + ;; # Avec un + car en $MODERNITE 5, nous sommes appelés en direct, et non plus en second choix après une passe en + qui elle aura eu le loisir de fouiller tous les compilos disponibles.
+		*) fatal "# Je ne sais pas gérer langcxx($*)" ;;
+	esac
+}
+
 cxx17()
 {
 	compiloSysVersion -i "$@" "clang >= 5" "gcc >= 7"
