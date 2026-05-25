f00de1dc753e1a8a248d4ca1062223466668f818
Guillaume Outters <guillaume-installs@outters.eu>
2026-05-25 23:17:46 +0200

    util.jalons
    + Dépose de jalons, jouant le rôle de cache pour reprendre à ladite étape en cas de plantage.
    
    darcs-hash:371ef738f1282c28e397da17a05b33e5c0c22fab

---
+++ util.jalons.sh
@@ -0,0 +1,56 @@
+# Copyright (c) 2026 Guillaume Outters
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Création de jalons pour reprendre un GuiLI vautré en plein vol, grâce à un cache de l'environnement (dont le dossier courant).
+# Utilisation:
+#	if ! jalon source ; then # Pas encore en cache?
+#		obtenirEtAllerDansVersion
+#		for modif in true $modifs ; do $modif ; done
+#		jalonner source # Mémorisation pour pouvoir court-circuiter la prochaine fois.
+#	fi
+# Le but est de pouvoir rattraper une compilation foirée sans tout reprendre à zéro;
+# notamment, lorsque la phase de tar + $modifs touche à un .h qui entraîne la recompil de TOUT le logiciel, on est bien contents de pouvoir simplement repartir de la phase make de juste ce qui manquait.
+
+# À FAIRE: des accroches pour les fonctions qui modifient ailleurs que les sources et l'environnement (ex.: celles qui déposent des binaires de contournement dans $TMP/$$).
+
+jalon()
+{
+	local jalon="jalon.$1:`bn "$dest"`$GUILI_MOIRE" PWD=
+	eval jalon_$1="$jalon"
+	
+	[ -f "$TMP/$jalon" ] || return 1
+	
+	gris "Chargement du jalon $TMP/$jalon (cache)"
+	. "$TMP/$jalon"
+	case "$PWD" in "") jaune "# jalon sans \$PWD; tant pis, on rebascule sur la version longue" >&2 ; return 1 ;; esac
+	cd "$PWD"
+}
+
+jalonner()
+{
+	local jalon
+	eval jalon='"$jalon_'$1'"'
+	case "$jalon" in "") rouge "# jalonner() appelée sans jalon() préalable" >&2 ; return 1 ;; esac
+	
+	{
+		set # Inclut le PWD, normalement.
+		export -p # export -p est le seul qui marche à l'identique sur les /bin/sh de FreeBSD 15.0 ou 10.2, Mac OS X 10.13, et Raspbian sur un RPi3.
+	} > "$TMP/$jalon.tmp" && mv "$TMP/$jalon.tmp" "$TMP/$jalon"
+}
